8fc94bbc3a98988b8740b7a353c9d634c91f6227
Aleksey Tumbaev <leha.tumbaev@gmail.com>
2024-05-11 20:28:11 +0700

    initial commit

---
+++ src/main/java/com/example/springopenapiexample/SpringOpenapiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springopenapiexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOpenapiExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOpenapiExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springopenapiexample/SpringOpenapiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springopenapiexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOpenapiExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
